a207aa1cfc01b3a28e57b5929f76bc8b2b129cf4
Steven Li <Steven Li>
2017-03-13 23:50:00 -0400

    Changed data files path

--- loadtodb.py
+++ loadtodb.py
@@ -3,7 +3,7 @@ import json
 
 if __name__ == '__main__':
     conn = redis.Redis('127.0.0.1', 6379)
-    origin_list = pd.read_csv('/data/pure_chn_link1.csv', header=None)
+    origin_list = pd.read_csv('pure_chn_link.csv', header=None)
     for index, row in origin_list.iterrows():
         key, value = row
         result = conn.hget('rg_chinese', key)
